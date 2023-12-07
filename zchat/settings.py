b157ff20614626dc681c6ae16b79241dfc3e06fb
Zedsich <52614690+Zedsich@users.noreply.github.com>
2023-12-07 18:50:48 +0800

    Update settings.py
    removed secret keys

--- zchat/settings.py
+++ zchat/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e4p+6vhoe%v@9+_ncm=ocq0k8jo4h+^a_zq#et(k1^88vvfwq'
+SECRET_KEY = 'xxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
